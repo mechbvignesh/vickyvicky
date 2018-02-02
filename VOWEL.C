dffbbb47c4fa96f233605bab2c6913697e272b45
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-02 14:00:12 +0530

    VOWEL.C

---
+++ VOWEL.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the letter",a);
+scanf("%d",&a);
+if(a=='a' a=='e' a=='i' a=='o' a=='u')
+printf("vowel");
+else
+printf("consonant");
+return 0;
+}
