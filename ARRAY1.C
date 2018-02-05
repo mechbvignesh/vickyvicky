6d24d7bb818b09dcafd290c9e1ec495a5c8facd4
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-05 18:14:28 +0700

    ARRAY1.C

---
+++ ARRAY1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    clrscr();
+     printf("Enter the first number");
+     scanf("%d",&a);
+    printf("Enter the second number ");
+    scanf("%d",&b);
+    for(i=1; i <=b;i++)
+    {
+     sum=sum+arr[i];
+     printf("%d",arr[i];);
+    }
+    printf("Sum = %d",sum);
+    getch();
+    return 0;
