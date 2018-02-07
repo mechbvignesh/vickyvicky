502d2c61c5b7ce132ef2fa831b8fd666ca297e55
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-07 16:42:01 +0530

    PRIME.C

---
+++ PRIME.C
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+int x,z;
+printf("enter the number");
+scanf("%d",%x);
+for(z=1;z<=x;z++);
+{
+if(x%z=0)
+}
+printf("\nit is prime number");
+else
+printf("\nit is not prime number");
+return 0;
+}
