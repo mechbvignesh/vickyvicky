a2eac781b3439dc55635103b0292de6c186796aa
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-07 16:22:49 +0530

    PALINDROME.C

---
+++ PALINDROME.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("enter the number");
+scanf("%d",&a);
+b=(a/100);
+c=(a-(b*100))/10;
+d=a-(b*100+c*10);
+e(d*100+c*10+b*b);
+if(a++e)
+printf("it is palindrome");
+else
+printf("it is not palindrome");
+return 0;
+}
