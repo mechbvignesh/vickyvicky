f91a93e239c8b1400c6f338b3670b432096a3f3b
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-07 15:52:03 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+int count=0;
+printf("enter the number;");
+scanf("%d".&a);
+while(a=0)
+{
+a\=10;
+count++;
+}
+printf("number of digit %d",count);
+return 0;
+}
+
+
