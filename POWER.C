e5c1ff9ddfd2c8c0d5d255204b32d9bbe5023d39
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-07 16:03:20 +0530

    POWER.C

--- POWER.C
+++ POWER.C
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int x,y,z;
+  printf("enter the value\n");
+  scanf("%d",&x);
+  printf("enter the value\n");
+  scanf("%d",&y);
+  z=power(x,y);
+  printf("%d\n",&z)
+    return 0
+}
