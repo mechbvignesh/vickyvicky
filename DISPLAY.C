bb36948f51c3ae10ec1d9d2af869a0a2d64643ed
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-02-09 13:28:16 +0530

    DISPLAY.C

--- DISPLAY.C
+++ DISPLAY.C
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+int i,j;
+printf("Enter The number till:");
+scanf("%d",&j);
+printf("Odd numbers between the limits are %d\n",j);
+for(i=0;i<=j;i++)
+{
+if((i%2)!=0)
+{
+printf("%d\n",i);
+}
+}
+return 0;
