e81e5e2b24f2c5e01e7cb1f89f7b9a0475bde407
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-03-02 13:21:40 +0530

    VR42.C

---
+++ VR42.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	int i;
+	char a[100],b[100];
+	for(i=0;a[i]!='\0';i++)
+	{
+	if(a[i]>b[i])
+	{
+		printf("%s is greater",a);
+		break;
+	}
+	else if(b[i]>a[i])
+	{
+		printf("%s is greater",b);
+		break;
+	}
+	else if
+}
+		printf("both are same");
+		break;
+	}
+	}
+	return 0;
+}
