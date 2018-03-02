6b52685ee4dee5a673e049ffff33977b637714d5
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-03-02 13:32:29 +0530

    VR43.C

--- VR43.C
+++ VR43.C
@@ -2,18 +2,18 @@
 #include<string.h>
 int main()
 {
-	char x[50],y[50],i,j;
-	scanf("%s",&x);
-	scanf("%s",&y);
-	for(i=0;x[i]!='\0';++i)
+	char a[50],b[50],i,j;
+	scanf("%s",&a);
+	scanf("%s",&b);
+	for(i=0;a[i]!='\0';++i)
 	{
-		for(i=0;y[j]!='\0';++j,++i)
+		for(i=0;b[j]!='\0';++j,++i)
 		{
-			x[i]=y[j];
+			a[i]=b[j];
 		}
-		x[i]='\0';
+		a[i]='\0';
 	}
-	printf("%s",x);
+	printf("%s",a);
   return 0;
   getch();
 }
