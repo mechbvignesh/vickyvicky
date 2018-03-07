7632e281c0e2eba0500d2c177c94b23b08b1205e
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-03-07 15:59:42 +0530

    52.7\3.C

---
+++ 52.7\3.C
@@ -0,0 +1,51 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int Q;
+	printf("\n enter the number from 1 to 12");
+	scanf("%d",&Q);
+	switch(Q)
+	{
+		case 1:
+		printf("\none");
+		break;
+		case 2:
+		printf("\ntwo");
+		break;
+		case 3:
+		printf("\nthree");
+		break;
+		case 4:
+		printf("\nfour");
+		break;
+		case 5:
+		printf("\nfive");
+		break;
+		case 6:
+		printf("\nsix");
+		break;
+		case 7:
+		printf("\nseven");
+		break;
+		case 8:
+		printf("\neight");
+		break;
+		case 9:
+		printf("\nnine");
+		break;
+		case 10:
+		printf("\nten");
+		break;
+		case 11:
+		printf("\neleven");
+		break;
+		case 12:
+		printf("\ntwelve");
+		break;
+		default:
+	printf("None");
+	}
+ 
+	return 0;
+}
