f471bd886e830a02c558b2688bb489cf31602f2a
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-03-16 03:22:08 -0700

    71.16\3.c

---
+++ 71.16\3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char str[50],temp[10];
+	int i,j,a,b;
+	printf("enter the string:");
+	scanf("%s",str);
+	a=strlen(str);
+	while(i<j)
+	{
+		for(i=0;i<a;i++)
+		{
+			for(j=a;j>=1;j--)
+			{
+				temp[b]=str[i];
+				str[i]=str[j];
+				str[j]=str[i];
+			}
+		}
+	}
+	printf("\nyes");
+	return 0;
+}
