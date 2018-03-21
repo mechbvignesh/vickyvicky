a4e48d2eeb765b28981f246a38d63811598e5da9
mechbvignesh <35688466+mechbvignesh@users.noreply.github.com>
2018-03-21 16:27:39 +0530

    85.21\3.c

---
+++ 85.21\3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	int b,i;
+	scanf("%s",&s);
+	b=strlen(s);
+	for(i=0;i<=b;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%c",s[i]);
+		}
+	}
+	printf("\t");
+	for(i=0;i<=b;i++)
+	{
+	if(i%2!=0)
+	{
+		printf("%c",s[i]);
+	}
+	}
+	return 0;
+}
